a9500e8a1fa7a21ca7240aac6c728454dd85f5a4
Phan Binh <binhphan0872006@gmail.com>
2026-07-31 17:32:02 +0700

    hoan thien saveTask va 1 phan loadTask

--- src/main/java/com/example/task_tracker/repository/TaskRepository.java
+++ src/main/java/com/example/task_tracker/repository/TaskRepository.java
@@ -9,12 +9,17 @@ import java.util.ArrayList;
 
 public class TaskRepository {
     
-    private final String FILE_PATH = "tasks.json";
     private final Path path = Paths.get("tasks.json");
 
     public List<Task> loadTasks() {
         List<Task> tasks = new ArrayList<>();
-        
+        if (!Files.exists(path)){
+            return tasks;
+        }
+        try {
+            String json = Files.readString(path).trim();
+            
+        }
         // TODO: 1. Kiểm tra file có tồn tại không
         // TODO: 2. Đọc toàn bộ nội dung file thành 1 chuỗi String (dùng Files.readString)
         // TODO: 3. Viết thuật toán cắt chuỗi JSON đó ra để trích xuất dữ liệu
@@ -29,12 +34,23 @@ public class TaskRepository {
             Task task = tasks.get(i);
             jsonBuild.append("  {\n");
             jsonBuild.append("    \"id\":").append(task.getId()).append(",\n");
-            jsonBuild.append("    ")
+            String safeDes = task.getDescription().replace("\"", "\\\"");
+            jsonBuild.append("    \"description\":\"").append(safeDes).append("\",\n");
+            jsonBuild.append("    \"status\":\"").append(task.getStatus()).append("\",\n");
+            jsonBuild.append("    \"createdAt\":\"").append(task.getCreatedAt().toString()).append("\",\n");
+            jsonBuild.append("    \"updatedAt\":\"").append(task.getUpdatedAt().toString()).append("\"\n");
+            jsonBuild.append("  }");
+            if (i < tasks.size()-1){
+                jsonBuild.append(",");
+            }
+            jsonBuild.append("\n");
         }
-        jsonBuild.append("\n]");
-        // TODO: 1. Tạo một StringBuilder để chứa chuỗi JSON
-        // TODO: 2. Dùng vòng lặp duyệt qua biến tasks
-        // TODO: 3. Lắp ráp từng thuộc tính (id, description...) thành chuỗi định dạng JSON
-        // TODO: 4. Ghi đè chuỗi đó xuống file (dùng Files.writeString)
+        jsonBuild.append("]");
+
+        try{
+            Files.writeString(path, jsonBuild.toString());
+        }catch (Exception ex){
+            System.out.println("Loi ghi file: " + ex.getMessage());
+        }
     }
 }
